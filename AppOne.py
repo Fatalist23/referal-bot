53be9e0c16e37efedac43bdc03a137c090c83349
zazafaka <230298Dan>
2018-02-09 16:59:14 +0300

    Add hello

--- AppOne.py
+++ AppOne.py
@@ -3,6 +3,7 @@ import mysql.connector
 updater = Updater(token='546132218:AAGvBSF7dI0QTxKLvwLFlKD0aaFXYBp2_Nc')
 dispatcher = updater.dispatcher
 
+# hello world
 def searchUser (usID):
     result = -1
     cursor.execute("SELECT counter FROM log WHERE id = (%s)",(usID))
